45a01107466ecbbc408cdfe2ccac2eeefb845254
Bibek kumar pandit <127099634+Bibek5120@users.noreply.github.com>
2025-05-06 17:01:47 +0530

    Add files via upload

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import FeaturesDestination from '../Components/FeaturesDestination'
+import ExclusiveOffer from '../Components/ExclusiveOffer'
+import Testimonial from '../Components/Testimonial'
+import NewsLetter from '../Components/NewsLetter'
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+     <FeaturesDestination />
+     <ExclusiveOffer/>
+     <Testimonial />
+     <NewsLetter />
+    </>
+  )
+}
+
+export default Home
